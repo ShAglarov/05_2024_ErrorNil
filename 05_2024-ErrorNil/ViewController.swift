e7d34a88d0b791561318f25ccd0071cac976f90d
ShAglarov <shamil.aglarov@gmail.com>
2024-03-26 21:00:35 +0300

    Initial Commit

---
+++ 05_2024-ErrorNil/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  05_2024-ErrorNil
+//
+//  Created by Shamil Aglarov on 26.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
